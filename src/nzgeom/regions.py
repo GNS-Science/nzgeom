a85223ed055c24701bc119fe933246f4270b772e
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-06-14 13:01:38 +1200

    include data and regions module in package distribution
    - restructure package directory tree to use a src/ folder within the
    package root.
    
        - something about the combination of the ['flat-layout/adhoc'](https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#flat-layout) package directory structure and setup.cfg did not work to make it install the data and also access the data from a different python working directory.  I tried a lot of combinations of packages, package_dir, options.package.find, and options.package.data within setup.cfg.  No joy.
        - [[https://setuptools.pypa.io/en/latest/userguide/datafiles.html#subdirectory-for-data-files][setuptool docs]] do not include an example of including a data directory with the so-called "flat-layout" directory structure.  The docs *do* include an example using a src directory within the package root.  I tried this and it *does* work.
        - I don't love the src directory approach - it adds a layer of complication.
          But [[https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure][this person]] says it's better, and it is working for my data, so I'm
          going with it (for now, at least).

---
+++ src/nzgeom/regions.py
@@ -0,0 +1,34 @@
+from importlib.resources import files
+from pathlib import Path
+import geopandas as gpd
+
+
+class NZRegions(object):
+    """container class for NZ region polygons"""
+
+    def __init__(self):
+        fname = files("nzgeom.data").joinpath(
+            "regions/regional-council-2018-generalised.gpkg"
+        )
+        if not Path(fname).exists:
+            raise FileNotFoundError(f"{fname} does not exist")
+        self.gdf = gpd.read_file(fname)
+
+    def list_regions(self):
+        """print the available region names"""
+        print(self.gdf.REGC2018_V1_00_NAME.to_string(index=False))
+
+    def get_region_geodataframe(self, region_name):
+        """produce GeoDataFrame containing a polygon representing a specified region of NZ"""
+        if not self.gdf.REGC2018_V1_00_NAME.str.contains(region_name).any():
+            raise ValueError(
+                f"{region_name} not available. Use list_regions() method to show list"
+                " of available regions."
+            )
+        return self.gdf.loc[self.gdf.REGC2018_V1_00_NAME == region_name]
+
+    def get_region_polygon(self, region_name):
+        """return a shapely polygon representing a specified region of NZ"""
+        reg_gdf = self.get_region_geodataframe(region_name)
+        assert reg_gdf.shape[0] == 1
+        return reg_gdf.geometry.iloc[0]
