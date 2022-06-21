27dcaa8c0104c385afe440d2265c35d86e734f41
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-06-21 15:00:44 +1200

    remove spatialpandas dependency
    - I included spatial pandas to provide the option to return polygons as
    a spatialpandas.GeoDataFrame object that holoviz can plot directly.
    This also allowed me to include a context manager to ignore shapely
    deprecation warnings triggered in holoviz once here, instead of having
    to create such a context manager in every piece of code that tries to
    plot these geometries using holoviews.
    
    However, requiring spatialpandas brings in as dependencies the entire
    holoviz ecosystem, and this raised the time to complete conda-build from
    a few minutes to about eight hours.  This tradeoff isn't worth it, so
    I'm removing the dependency.

--- src/nzgeom/coastlines.py
+++ src/nzgeom/coastlines.py
@@ -1,6 +1,5 @@
 from importlib.resources import files
 import geopandas as gpd
-import spatialpandas as spd
 
 # EPSG:4326 - WGS 84, latitude/longitude coordinate system based on the Earth's
 # center of mass, used by the Global Positioning System among others.
@@ -13,41 +12,9 @@ def _geopackage_to_gpd_geodataframe(fname):
     return gdf
 
 
-def _geopackage_to_spd_geodataframe(fname):
-    """return a spatialpandas GeoDataFrame containing the NZ coastline
-
-    `spatialpandas <https://github.com/holoviz/spatialpandas>`_ is designed to
-    work nicely with `holoviz <https://holoviz.org/>`_.
-
-    """
-    gdf = _geopackage_to_gpd_geodataframe(fname)
-
-    # filter out shapely deprecation warning triggered by spatialpandas
-    # https://shapely.readthedocs.io/en/stable/migration.html#creating-numpy-arrays-of-geometry-objects
-    import warnings
-    from shapely.errors import ShapelyDeprecationWarning
-
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
-        return spd.GeoDataFrame(gdf)
-
-
-def get_NZ_coastlines(fmt: str = "geopandas"):
-    """return a GeoDataFrame containing the NZ coastline
-
-    The format of the GeoDataFrame can be either `geopandas
-    <https://geopandas.org/>`_ (default) or `spatialpandas
-    <https://github.com/holoviz/spatialpandas>`_.
-
-    ARGS:
-    fmt: string: {"geopandas"} | "spatialpandas"
-    """
+def get_NZ_coastlines():
+    """return a geopandas.GeoDataFrame containing the NZ coastline"""
     fname = files("nzgeom.data").joinpath(
         "coastlines/nz-coastlines-and-islands-polygons-topo-150k.gpkg"
     )
-    if fmt == "geopandas":
-        return _geopackage_to_gpd_geodataframe(fname)
-    elif fmt == "spatialpandas":
-        return _geopackage_to_spd_geodataframe(fname)
-    else:
-        raise ValueError('fmt must be on of ["geopandas", "spatialpandas"]')
+    return _geopackage_to_gpd_geodataframe(fname)
