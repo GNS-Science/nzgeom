cd7054e06c9d46503cc4376377bc9c57f0bac34e
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-06-28 12:06:27 +1200

    setup environment for testing using tox, pytest

---
+++ src/tests/nzgeom_test.py
@@ -0,0 +1,11 @@
+def test_coastline_import():
+    import nzgeom.coastlines as coasts
+
+    # c = nzgeom.coastlines.get_NZ_coastlines()
+
+
+def test_regions_import():
+    import nzgeom.regions as regions
+
+    # r = nzgeom.regions.NZRegions()
+    # r.list_regions()
