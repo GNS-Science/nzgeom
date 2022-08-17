7c48dbc78a331eca75d73980ef304b7e99c7d966
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-08-17 14:19:44 +1200

    option to exclude Chatham, Kermadec islands
    The Chatham Islands and the Kermadec Islands are east of 180 degress
    longitude. Including these islands in a plot of New Zealand causes the
    plot's horizontal axis to span roughly -177 deg E to 177 deg E, that is,
    the whole world.  This is usually undesirable.

--- src/nzgeom/coastlines.py
+++ src/nzgeom/coastlines.py
@@ -6,15 +6,45 @@ import geopandas as gpd
 LATLON = "EPSG:4326"  # https://epsg.io/4326
 
 
-def _geopackage_to_gpd_geodataframe(fname):
-    """return a geopandas GeoDataFrame containing the NZ coastline"""
+def _geopackage_to_gpd_geodataframe(fname: str) -> gpd.GeoDataFrame:
+    """return a geopandas GeoDataFrame containing the NZ coastline
+
+    helper function for get_NZ_coastlines()
+    """
     gdf = gpd.read_file(fname).to_crs(LATLON)
     return gdf
 
 
-def get_NZ_coastlines():
-    """return a geopandas.GeoDataFrame containing the NZ coastline"""
+def get_NZ_coastlines(
+    include_chatham_islands: bool = False, include_kermadec_islands: bool = False
+) -> gpd.GeoDataFrame:
+    """return a geopandas.GeoDataFrame containing the NZ coastline.
+
+    ARGS:
+        include_chatham_islands: if true, include the coastline of the Chatham
+            Islands in the returned geodataframe.
+        include_kermadec_islands: if true, include the coastline of the Kermadec
+            Islands in the returned geodataframe.
+
+    RETURNS:
+        a `geopandas.GeoDataFrame
+        <https://geopandas.org/en/stable/docs/user_guide/data_structures.html#geodataframe>`_
+        object containing multipolygons representing New Zealand's coastlines.
+
+    NOTES:
+
+    The Chatham Islands and the Kermadec Islands are east of 180 degress
+    longitude. Including these islands in a plot of New Zealand causes the
+    plot's horizontal axis to span roughly -177 deg E to 177 deg E, that is, the
+    whole world.
+
+    """
     fname = files("nzgeom.data").joinpath(
         "coastlines/nz-coastlines-and-islands-polygons-topo-150k.gpkg"
     )
-    return _geopackage_to_gpd_geodataframe(fname)
+    gdf = _geopackage_to_gpd_geodataframe(fname)
+    if not include_kermadec_islands:
+        gdf = gdf.loc["Kermadec" not in gdf["grp_name"]]
+    if not include_chatham_islands_islands:
+        gdf = gdf.loc["Chatham" not in gdf["grp_name"]]
+    return gdf
